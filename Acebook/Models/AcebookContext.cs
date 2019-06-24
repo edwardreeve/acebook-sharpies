7dd2fc332301e91abcf20f6a84424af58ed33baa
edwardreeve <edreeve+git@gmail.com>
2019-06-24 17:01:09 +0100

    Migrations automatically run with startup

--- Acebook/Models/AcebookContext.cs
+++ Acebook/Models/AcebookContext.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 namespace Acebook.Models {
   public class AcebookContext : DbContext {
     public AcebookContext (DbContextOptions<AcebookContext> options) : base (options) { }
-    public DbSet<Post> Acebook { get; set; }
+    public DbSet<Post> Post { get; set; }
 
   }
 }
