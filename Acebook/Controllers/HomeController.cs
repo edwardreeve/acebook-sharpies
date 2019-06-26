5c60a3395fcaece0207158d489a9056108eca544
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-06-26 12:12:38 +0100

    written first feature test to check home page

--- Acebook/Controllers/HomeController.cs
+++ Acebook/Controllers/HomeController.cs
@@ -3,35 +3,30 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Acebook.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Acebook.Controllers
-{
-    public class HomeController : Controller
-    {
-        private readonly AcebookContext _context;          
-        public HomeController(AcebookContext context)         
-        {             
-            _context = context; 
-        }    
-
-        [HttpGet]        
-        public IActionResult Index()
-        {
-            List<Post> posts = _context.Post.OrderByDescending(x => x.CreatedAt).ToList();
-            return View(posts);
+namespace Acebook.Controllers {
+    [Route("[controller]")]
+    public class HomeController : Controller {
+        private readonly AcebookContext _context;
+        public HomeController (AcebookContext context) {
+            _context = context;
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
+        [HttpGet]
+        public IActionResult Index () {
+            List<Post> posts = _context.Post.OrderByDescending (x => x.CreatedAt).ToList ();
+            return View (posts);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy () {
+            return View ();
+        }
+
+        [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error () {
+            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Acebook/Migrations/20190625125336_completeDb2.cs
+++ Acebook/Migrations/20190625125336_completeDb2.cs
@@ -2,49 +2,41 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
-namespace Acebook.Migrations
-{
-    public partial class completeDb2 : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
+namespace Acebook.Migrations {
+    public partial class completeDb2 : Migration {
+        protected override void Up (MigrationBuilder migrationBuilder) {
+            migrationBuilder.CreateTable (
                 name: "Post",
-                columns: table => new
-                {
-                    Id = table.Column<long>(nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
-                    UserId = table.Column<long>(nullable: false),
-                    Body = table.Column<string>(nullable: true),
-                    CreatedAt = table.Column<DateTime>(nullable: false)
+                columns : table => new {
+                    Id = table.Column<long> (nullable: false)
+                        .Annotation ("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                        UserId = table.Column<long> (nullable: false),
+                        Body = table.Column<string> (nullable: true),
+                        CreatedAt = table.Column<DateTime> (nullable: false)
                 },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Post", x => x.Id);
+                constraints : table => {
+                    table.PrimaryKey ("PK_Post", x => x.Id);
                 });
 
-            migrationBuilder.CreateTable(
+            migrationBuilder.CreateTable (
                 name: "User",
-                columns: table => new
-                {
-                    Id = table.Column<long>(nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
-                    Name = table.Column<string>(nullable: true),
-                    Email = table.Column<string>(nullable: true),
-                    Password = table.Column<string>(nullable: true)
+                columns : table => new {
+                    Id = table.Column<long> (nullable: false)
+                        .Annotation ("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                        Name = table.Column<string> (nullable: true),
+                        Email = table.Column<string> (nullable: true),
+                        Password = table.Column<string> (nullable: true)
                 },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_User", x => x.Id);
+                constraints : table => {
+                    table.PrimaryKey ("PK_User", x => x.Id);
                 });
         }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
+        protected override void Down (MigrationBuilder migrationBuilder) {
+            migrationBuilder.DropTable (
                 name: "Post");
 
-            migrationBuilder.DropTable(
+            migrationBuilder.DropTable (
                 name: "User");
         }
     }
