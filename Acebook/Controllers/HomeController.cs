aae7892b5f9f221ac17fb4e4045edaadfc80c515
edwardreeve <edreeve+git@gmail.com>
2019-06-27 15:32:27 +0100

    Starting from scratch, trying to have multiple models in one view

--- Acebook/Controllers/HomeController.cs
+++ Acebook/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using Acebook.Models;
-using Microsoft.AspNetCore.Mvc;
 using BCrypt.Net;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace Acebook.Controllers {
     public class HomeController : Controller {
@@ -20,45 +22,49 @@ namespace Acebook.Controllers {
             return View (posts);
         }
 
+        // [HttpGet]
+        // public IActionResult CommentList (long id) {
+        //     List<Comment> PostComments = _context.Comment.Where (c => c.postId == id).ToList ();
+        //     // List<Comment> comments = _context.Comment.OrderByDescending (x => x.CreatedAt).ToList ();
+        //     ViewData["Comments"] = PostComments;
+        //     return View ();
+        //  }
+
         [HttpPost]
-        public async Task<ActionResult> CreatePost(string body, long userId = 1)
-        {
-            Post post = new Post();
-            Post newPost = post.CreatePost(body, userId);
+        public async Task<ActionResult> CreatePost (string body, long userId = 1) {
+            Post post = new Post ();
+            Post newPost = post.CreatePost (body, userId);
 
-            _context.Post.Add(newPost);
-            await _context.SaveChangesAsync();
+            _context.Post.Add (newPost);
+            await _context.SaveChangesAsync ();
 
-            return Redirect("/");
+            return Redirect ("/");
         }
 
         public IActionResult Privacy () {
             return View ();
         }
-        
-        public IActionResult Signup()
-        {
-            return View();
+
+        public IActionResult Signup () {
+            return View ();
         }
-        
-        [HttpPost("post")]
-        public async Task<ActionResult<string>> PostUser(User user)
-        {
-            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
-            _context.User.Add(user);
-            await _context.SaveChangesAsync();
+
+        [HttpPost ("post")]
+        public async Task<ActionResult<string>> PostUser (User user) {
+            user.Password = BCrypt.Net.BCrypt.HashPassword (user.Password);
+            _context.User.Add (user);
+            await _context.SaveChangesAsync ();
 
             return "Welcome!";
         }
 
         [HttpPost]
-        public async Task<ActionResult> CreateComment(Comment comment)
-        {
+        public async Task<ActionResult> CreateComment (Comment comment) {
             comment.CreatedAt = DateTime.Now;
-            _context.Comment.Add(comment);
-            await _context.SaveChangesAsync();
+            _context.Comment.Add (comment);
+            await _context.SaveChangesAsync ();
 
-            return Redirect("/");
+            return Redirect ("/");
         }
 
         [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
