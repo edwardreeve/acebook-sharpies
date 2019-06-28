5a1459c41013c46d3664b1cab3d66681b709147f
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-06-28 14:15:07 +0100

    name on post and comments

---
+++ Acebook/Migrations/20190628123139_ForeignKeyTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Acebook.Migrations
+{
+    public partial class ForeignKeyTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_UserId",
+                table: "Post",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_User_UserId",
+                table: "Post",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_User_UserId",
+                table: "Post");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Post_UserId",
+                table: "Post");
+        }
+    }
+}
